f95d62555eec2bd834ac6d88fbfe78e005497ca7
Calvin McDowell <calvinmac40@hotmail.com>
2019-11-12 22:04:06 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,10 +1,10 @@
 def unsafe?(speed)
   if speed > 60
-    return true 
-  elsif speed < 40
+    return true
+  elsif speed < 40 
     return true
   else speed == 50
-    return false 
+    return false
   end
 end
 
